494728186b2545959e461493a084851f694e1105
Tsuki <yiyuec261@gmail.com>
2026-05-25 12:13:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Welcome to Tsuki Universe");
+for (let i = 0; i < 80; i++) {
+    let star = document.createElement("div");
+    star.className = "star";
+
+    star.style.left = Math.random() * 100 + "vw";
+    star.style.top = Math.random() * 100 + "vh";
+    star.style.animationDuration = Math.random() * 3 + 2 + "s";
+
+    document.body.appendChild(star);
+}
